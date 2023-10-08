ee342c4d67d206153dee780b553de3054dce4301
Sumanth2118 <147347522+Sumanth2118@users.noreply.github.com>
2023-10-08 23:24:56 +0200

    Update sensor.py

--- homeassistant/components/iaqualink/sensor.py
+++ homeassistant/components/iaqualink/sensor.py
@@ -2,7 +2,6 @@
 from __future__ import annotations
 
 from homeassistant.components.sensor import DOMAIN, SensorDeviceClass, SensorEntity
-from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import UnitOfTemperature
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
@@ -15,7 +14,6 @@ PARALLEL_UPDATES = 0
 
 async def async_setup_entry(
     hass: HomeAssistant,
-    config_entry: ConfigEntry,
     async_add_entities: AddEntitiesCallback,
 ) -> None:
     """Set up discovered sensors."""
