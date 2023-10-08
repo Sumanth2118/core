b70015177ec1e78ce96b4a188a9b7e3cdba30c8a
Sumanth2118 <147347522+Sumanth2118@users.noreply.github.com>
2023-10-08 23:54:23 +0200

    Update remote.py

--- homeassistant/components/itach/remote.py
+++ homeassistant/components/itach/remote.py
@@ -25,7 +25,7 @@ from homeassistant.const import (
 from homeassistant.core import HomeAssistant
 import homeassistant.helpers.config_validation as cv
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
-from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
+from homeassistant.helpers.typing import ConfigType
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -71,10 +71,8 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
 
 
 def setup_platform(
-    hass: HomeAssistant,
     config: ConfigType,
     add_entities: AddEntitiesCallback,
-    discovery_info: DiscoveryInfoType | None = None,
 ) -> None:
     """Set up the ITach connection and devices."""
     itachip2ir = pyitachip2ir.ITachIP2IR(
